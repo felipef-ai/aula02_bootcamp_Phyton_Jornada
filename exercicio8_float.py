4e31d9468b52fe218c1cc68c335687e1ba946616
Felipe Fernandes <felipefernandes.mkt@gmail.com>
2025-02-11 17:56:50 -0300

    adicionando o exercicio8_float.py

---
+++ exercicio8_float.py
@@ -0,0 +1,6 @@
+#8 Desenvolva um programa que calcule a potência de um número (base e expoente fornecidos pelo usuário).
+print("Vamos calcular a potência de um número, siga abaixo a nossa intrução!!!")
+base = float(input("Digite um número base: "))
+expoente = float(input("Digite um número expoente: "))
+potencia = base ** expoente
+print("A potência é: ", potencia)
