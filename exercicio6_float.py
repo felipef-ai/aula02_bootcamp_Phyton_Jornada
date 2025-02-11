2b2a7229902bfc92d65b86ca4d650c768633713c
Felipe Fernandes <felipefernandes.mkt@gmail.com>
2025-02-11 17:35:51 -0300

    adicionado o exercicio6_float.py

---
+++ exercicio6_float.py
@@ -0,0 +1,6 @@
+#6Escreva um programa que receba dois números flutuantes e realize sua adição.
+
+num_1 = float(input("Digite um numero flutuante: "))
+num_2 = float(input("Digite outro numero flutuante: "))
+soma_total = num_1 + num_2
+print("A soma dos números flutuantes é: ", soma_total)
