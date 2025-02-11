a78485298870fe3eee6219063eebc4cb8a8845a0
Felipe Fernandes <felipefernandes.mkt@gmail.com>
2025-02-11 18:09:08 -0300

    adicionado o exercicio9_float.py

---
+++ exercicio9_float.py
@@ -0,0 +1,6 @@
+# 9 - Faça um programa que converta a temperatura de Celsius para Fahrenheit.
+
+print("Vamos converter a temperatura de Celsius para Fahrenheit, para isso, siga as instruções abaixo.")
+celsius = float(input("Por favor, digite a temperatura atual em Graus Celsius: "))
+fahrenheit = (celsius * 9/5) + 32
+print(f"A temperatura de {celsius}ºC equivale a {fahrenheit}ºF")
