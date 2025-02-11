1d19e9e3d070ed0fc6b911c0c3c2e70eed806c15
Felipe Fernandes <felipefernandes.mkt@gmail.com>
2025-02-11 15:03:53 -0300

    adicionando o exercicio2_int.py

---
+++ exercicio2_int.py
@@ -0,0 +1,5 @@
+#2-Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
+
+num_1 = int(input("Digite um numero: "))
+resultado = num_1 % 5
+print("O resto da divisão por 5 é: ", resultado)
