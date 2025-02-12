e60105a7f384594f50c7d0f64f31dd430a1967cb
Felipe Fernandes <felipefernandes.mkt@gmail.com>
2025-02-12 14:40:29 -0300

    adicionando o exercicio11_stri.py

---
+++ exercicio11_stri.py
@@ -0,0 +1,5 @@
+#11  -Escreva um programa que receba uma string do usuário e a converta para maiúsculas.
+
+nome_usuario = input("Digite seu nome: ")
+convert_maiusculas = nome_usuario.upper()
+print(f"O seu nome em letras maíusculas é {convert_maiusculas}")
