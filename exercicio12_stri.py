dc19e9ace9823df1e4e5c0b83365204457d518a3
Felipe Fernandes <felipefernandes.mkt@gmail.com>
2025-02-12 14:43:53 -0300

    adicionado o exercicio12_stri.py

---
+++ exercicio12_stri.py
@@ -0,0 +1,4 @@
+# 12 - Crie um programa que receba o nome completo do usuário e imprima o nome com todas as letras minúsculas.
+nome_usuario_completo = input("Digite seu nome e sobrenome: ")
+convert_maiusculas = nome_usuario_completo.upper()
+print(f"O seu nome completo em letras maíusculas é {convert_maiusculas}")
