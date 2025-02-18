58f86c658d2640531efbcc9b958cf2f8d45f6c9a
Felipe Fernandes <felipefernandes.mkt@gmail.com>
2025-02-17 21:34:21 -0300

    adicionado o exercicio18_bool.py

---
+++ exercicio18_bool.py
@@ -0,0 +1,4 @@
+# 18 Desenvolva um programa que peça ao usuário para inserir um valor booleano e, em seguida, inverta esse valor.
+valor = input("Digite True ou False: ")
+resultado_not = not valor
+print("Resultado do NOT lógico:", resultado_not)
