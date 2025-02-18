fb451fbdd861665c81aa3bed70b51e0660664309
Felipe Fernandes <felipefernandes.mkt@gmail.com>
2025-02-17 21:32:11 -0300

    adicionando o exercicio15_str.py

---
+++ exercicio15_stri.py
@@ -0,0 +1,7 @@
+#15 - Escreva um programa que concatene duas strings fornecidas pelo usuário.
+nome = input("Digite o seu primeiro nome: ")
+sobrenome = input("Digite o seu sobrenome: ")
+profissão = input("Digite a sua profissão: ")
+texto_concatenado= nome + " " + sobrenome
+print(f"Seu nome completo é {texto_concatenado} , e sua profissão é {profissão} . Parabéns!!!")
+
