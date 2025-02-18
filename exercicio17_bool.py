a6633159006126b6570f1b6932540e31c4f60a18
Felipe Fernandes <felipefernandes.mkt@gmail.com>
2025-02-17 21:33:48 -0300

    adicionado o exercicio17_bool.py

---
+++ exercicio17_bool.py
@@ -0,0 +1,5 @@
+# Crie um programa que receba dois valores booleanos do usuário e retorne o resultado da operação OR.
+valor1 = True
+valor2 = False
+resultado_or = valor1 or valor2
+print ("O resultado da operação é: ", resultado_or)
