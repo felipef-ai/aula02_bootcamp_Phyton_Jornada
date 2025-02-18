862f7da441f51519ada1aadc0647a2aa4daffa67
Felipe Fernandes <felipefernandes.mkt@gmail.com>
2025-02-17 21:35:11 -0300

    adicionado o exercicio19_bool.py

---
+++ exercicio19_bool.py
@@ -0,0 +1,5 @@
+# Faça um programa que compare se dois números fornecidos pelo usuário são iguais.
+num1 = input("Digite um número: ")
+num2 = input("Digite outro número: ")
+result = num1 == num2
+print("O resultado da igualdade é: ", result)
