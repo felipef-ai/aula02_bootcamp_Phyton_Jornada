01779c98665f7c4f362a7a7af1ceacde42520d7a
Felipe Fernandes <felipefernandes.mkt@gmail.com>
2025-02-17 21:35:47 -0300

    adicionado o exercicio20_bool.py

---
+++ exercicio20_bool.py
@@ -0,0 +1,5 @@
+# Escreva um programa que verifique se dois números fornecidos pelo usuário são diferentes.
+num1 = input("Digite um número: ")
+num2 = input("Digite outro número: ")
+result = num1 != num2
+print("O resultado da diferença é: ", result)
